b971b13af293a20fc92d654b80deafbd0b35c1ed
Shirin Mozaffari <shirinmozaffari@Shirins-MacBook-Air.local>
2024-03-30 18:57:50 -0400

    initial commit

---
+++ src/test/java/com/shirin/bloodbankmanagementsystem/BloodBankManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shirin.bloodbankmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodBankManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
